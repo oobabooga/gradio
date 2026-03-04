29bca5e4338cd47bd45f3185f5a36267f1b36de8
oobabooga <112222186+oobabooga@users.noreply.github.com>
2026-03-03 18:30:29 -0800

    Try to make .change() functional

--- js/headless/Index.svelte
+++ js/headless/Index.svelte
@@ -1,3 +1,16 @@
-<script>
-	export let value;
+<script lang="ts">
+	import type { Gradio } from "@gradio/utils";
+
+	export let value: any;
+	let old_value: any;
+	export let gradio: Gradio<{
+		change: never;
+	}>;
+
+	$: {
+		if (value !== old_value) {
+			old_value = value;
+			gradio.dispatch("change");
+		}
+	}
 </script>
