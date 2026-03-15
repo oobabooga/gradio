f63b180211b3acfa40f8d10166a5d12591fa9680
oobabooga <112222186+oobabooga@users.noreply.github.com>
2026-03-15 14:26:12 -0700

    Undo a change

--- gradio/routes.py
+++ gradio/routes.py
@@ -399,12 +399,13 @@ class App(FastAPI):
                 template = (
                     "frontend/share.html" if blocks.share else "frontend/index.html"
                 )
+                gradio_api_info = api_info(False)
                 return templates.TemplateResponse(
                     template,
                     {
                         "request": request,
                         "config": config,
-                        "gradio_api_info": None,
+                        "gradio_api_info": gradio_api_info,
                     },
                 )
             except TemplateNotFound as err:
